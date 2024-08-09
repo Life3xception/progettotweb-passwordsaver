e730037bd353af4329dc968461403dd6f2a8a548
Life3xception <life3xception@proton.me>
2024-08-09 11:39:00 +0200

    Added CORS filter

---
+++ src/main/java/passwordsaver/progettotwebpasswordsaver/cors/CorsFilter.java
@@ -0,0 +1,38 @@
+package passwordsaver.progettotwebpasswordsaver.cors;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "*")
+public class CorsFilter extends HttpFilter {
+    public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        String origin = request.getHeader("Origin");
+
+        if (!origin.startsWith("http://localhost/") && !origin.startsWith("http://localhost:"))
+            chain.doFilter(request, response);
+
+        // Authorize (allow) all domains to consume the content
+        response.addHeader("Access-Control-Allow-Credentials", "true");
+        response.addHeader("Access-Control-Allow-Origin", origin);
+        response.addHeader("Access-Control-Allow-Headers", "Content-Type");
+        response.addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST, PATCH, DELETE");
+
+
+        // For HTTP OPTIONS verb/method reply with ACCEPTED status code -- per CORS handshake
+        if (request.getMethod().equals("OPTIONS")) {
+            response.setStatus(HttpServletResponse.SC_ACCEPTED);
+            return;
+        }
+
+        // pass the request along the filter chain
+        chain.doFilter(request, response);
+    }
+}
